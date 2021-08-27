058b2f9f86386e3baf8025b724fe5022f60a3a39
Tasneem <tasneem@miramar.tech>
2021-08-27 08:59:17 +0200

    some fixes

--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -4,12 +4,14 @@ const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        const location = path.join("uploads",(req.user._id).toString())
+        //  path.join("uploads",(req.user._id).toString())
+        const location = path.join("uploads",'')
         fs.mkdir(location, (err)=>{})
         cb(null, location)
     },
     filename: function(req, file, cb){
         let myName = file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+
         cb(null, myName)
     }
 })
